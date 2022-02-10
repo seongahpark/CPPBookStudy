857be96c7f266ed7c49b9e8ab33c154dc8a311a2
park52 <wogus8660@gmail.com>
2022-02-10 23:53:11 +0900

    3주차 코드 추가 - item11.cpp

--- Effective_CPP/Week03/Park52/item11.cpp
+++ Effective_CPP/Week03/Park52/item11.cpp
@@ -1,18 +1,62 @@
-class Widget{
-    void swap(Widget& rhs);
+// class Widget{
+//     void swap(Widget& rhs);
+// };
+
+// Widget& Widget::operator=(const Widget& rhs)
+// {
+//     Widget temp(rhs);
+//     swap(temp);
+
+//     return *this;
+// }
+
+// Widget& Widget::operator=(Widget rhs)
+// {
+//     swap(rhs);
+
+//     return *this;
+// }
+
+#include <iostream>
+
+class Bitmap {};
+class Widget {
+public:
+	Widget();
+	~Widget();
+	Widget& operator=(const Widget &rhs);
+private:
+	Bitmap *pb;
 };
 
-Widget& Widget::operator=(const Widget& rhs)
+Widget::Widget()
 {
-    Widget temp(rhs);
-    swap(temp);
-
-    return *this;
+	pb = new Bitmap();
 }
 
-Widget& Widget::operator=(Widget rhs)
+Widget::~Widget()
 {
-    swap(rhs);
+	if (nullptr != pb)
+	{
+		delete pb;
+		pb = nullptr;
+	}
+}
 
-    return *this;
+Widget& Widget::operator=(const Widget &rhs)
+{
+	Bitmap *pOrig = pb;
+	pb = new Bitmap(*rhs.pb);
+	delete pOrig;
+
+	return *this;
+}
+
+int main()
+{
+	Widget a;
+	Widget b;
+	a = b;
+
+	return 0;
 }
